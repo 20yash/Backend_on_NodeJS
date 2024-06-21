ca7c40aca2af24026dc94b6a3493272af9e4ca94
shivanshu pathak <kashyapy58@gmail.com>
2024-06-21 13:34:19 +0530

    connect to mongoAtlas

---
+++ src/index.js
@@ -0,0 +1,42 @@
+// require('dotenv').config({path:'./env'})//this will also work but this does not make our code consistent
+
+import dotenv from 'dotenv'
+
+// import mongoose from 'mongoose'
+// import {DB_NAME} from './constants'
+
+import connectDB from './db/index.js'
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB()
+
+//First approach to set up database
+
+// import express from 'express'
+// const app = express()
+// (async ()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error",(error)=>{
+//             console.log("cannot connect",error);
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`app is listening on PORT ${process.env.PORT}`);
+//         })
+        
+//     } catch (error) {
+//         console.log("error",error);
+//         throw error
+        
+//     }
+// }
+// )()//Using IIFE(immediately invoked function here),as the database will be the first thing to connect and execute
+
+
+//second approach is,creating a separate file in db folder and importing the function from there to index.js file
+//this second approach is much better
